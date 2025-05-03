bc63e938fd24321e3c32bb9331759dfdccbd2af3
whyxxh <rubenbeg0908@gmail.com>
2025-05-03 16:52:38 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rubs Compiler
+
+this is a recreational programming project where I attempt to make a compiler and see where it can go !
+
+### Quick TODO - Direction
+
+- [ ] make a basic compiler for arithmetic expressions
+- [ ] make a basic C subset compiler
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{ 
+    if (argc < 2 ) {
+        fprintf(stderr, "usage : ./rcc <file>\n");
+        return 1;
+    }
+
+    FILE *f = fopen(argv[1], "r");
+    if (!f) {
+        perror("failled to open file");
+    }
+    
+    return 0;
+}
